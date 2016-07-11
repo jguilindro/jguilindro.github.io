ac613679712e3be7dbed767588403f10a61ed841
jguilindro <julio.guilindro@gmail.com>
2016-07-11 06:50:07 -0500

    :sagittarius:

---
+++ src/modificar.js
@@ -0,0 +1,178 @@
+$(document).ready(function(){
+
+
+var lista= document.getElementById("dropboton");
+
+for (var i = centros.length - 1; i >= 0; i--) {
+	
+	var li= document.createElement("li");
+	var a= document.createElement ("a");
+	a.textContent= centros[i].Nombre;
+	li.appendChild(a);
+	console.log(centros);
+	lista.appendChild(li);
+};
+	
+
+});
+
+
+var centros = 
+	[
+		{
+			"id_centro":	"1",
+			"Nombre": 		"Mejor Salud",
+			"Ciudad":		"Guayaquil",
+			"Direccion": 	"Av. Juan Tanca Marengo",
+			"Descripcion":	"El mejor Centro médico de la ciudad",
+			"Examenes":		[
+								{
+									"Nombre": 	"Recuento de Células Sanguíneas",
+									"Tipo":		"Sangre",
+									"costo":	"45"
+			
+								},
+								{
+									"Nombre": 	"Química Sanguínea",
+									"Tipo":		"Sangre",
+									"costo":	"30"
+			
+								},
+								{
+									"Nombre": 	"Cultivo de Sangre",
+									"Tipo":		"Sangre",
+									"costo":	"70"
+			
+								},
+								{
+									"Nombre": 	"Prueba de Plomo",
+									"Tipo":		"Sangre",
+									"costo":	"20"
+			
+								},
+								{
+									"Nombre": 	"Funcionamiento del Hígado",
+									"Tipo":		"Sangre",
+									"costo":	"19"
+			
+								},
+							]
+		},
+		{
+			"id_centro":	"2",
+			"Nombre": 		"AlfaCentro",
+			"Ciudad":		"Guayaquil",
+			"Direccion": 	"Urdesa",
+			"Descripcion":	"El centro médico por excelencia",
+			"Examenes":		[
+								{
+									"Nombre": 	"Recuento de Células Sanguíneas",
+									"Tipo":		"Sangre",
+									"costo":	"45"
+			
+								},
+								{
+									"Nombre": 	"Química Sanguínea",
+									"Tipo":		"Sangre",
+									"costo":	"30"
+			
+								},
+								{
+									"Nombre": 	"Cultivo de Sangre",
+									"Tipo":		"Sangre",
+									"costo":	"70"
+			
+								},
+								{
+									"Nombre": 	"Prueba de Plomo",
+									"Tipo":		"Sangre",
+									"costo":	"20"
+			
+								},
+								{
+									"Nombre": 	"Funcionamiento del Hígado",
+									"Tipo":		"Sangre",
+									"costo":	"19"
+			
+								},
+							]
+		},
+		{
+			"id_centro":	"3",
+			"Nombre": 		"QuitoMedik",
+			"Ciudad":		"Quito",
+			"Direccion": 	"Centro Norte",
+			"Descripcion":	"Ubicado entre calle x y calle y",
+			"Examenes":		[
+								{
+									"Nombre": 	"Recuento de Células Sanguíneas",
+									"Tipo":		"Sangre",
+									"costo":	"45"
+			
+								},
+								{
+									"Nombre": 	"Química Sanguínea",
+									"Tipo":		"Sangre",
+									"costo":	"30"
+			
+								},
+								{
+									"Nombre": 	"Cultivo de Sangre",
+									"Tipo":		"Sangre",
+									"costo":	"70"
+			
+								},
+								{
+									"Nombre": 	"Prueba de Plomo",
+									"Tipo":		"Sangre",
+									"costo":	"20"
+			
+								},
+								{
+									"Nombre": 	"Funcionamiento del Hígado",
+									"Tipo":		"Sangre",
+									"costo":	"19"
+			
+								},
+							]
+		},
+		{
+			"id_centro":	"4",
+			"Nombre": 		"CuencaMedik",
+			"Ciudad":		"Cuenca",
+			"Direccion": 	"Centro Norte",
+			"Descripcion":	"Ubicado entre calle x y calle y",
+			"Examenes":		[
+								{
+									"Nombre": 	"Recuento de Células Sanguíneas",
+									"Tipo":		"Sangre",
+									"costo":	"45"
+			
+								},
+								{
+									"Nombre": 	"Química Sanguínea",
+									"Tipo":		"Sangre",
+									"costo":	"30"
+			
+								},
+								{
+									"Nombre": 	"Cultivo de Sangre",
+									"Tipo":		"Sangre",
+									"costo":	"70"
+			
+								},
+								{
+									"Nombre": 	"Prueba de Plomo",
+									"Tipo":		"Sangre",
+									"costo":	"20"
+			
+								},
+								{
+									"Nombre": 	"Funcionamiento del Hígado",
+									"Tipo":		"Sangre",
+									"costo":	"19"
+			
+								},
+							]
+		},
+	]
